61092c3a65dfd79f41648e87bdc57cc73c053970
Miguel Velásquez <vmiguel.xy@gmail.com>
2023-02-23 21:03:10 -0400

    Corrección del Log en Sequelize
    El mensaje de advertencia del commit anterior era debido a que, tal cual como indicaba el mensaje, la propiedad "logging" del segundo parámetro de la instancia sequelize estaba en "true", y por lo visto dicho parámetro debería estar ser false o una función.
    Por suerte en los aportes de la clase hubo un compañero (Juan Jacobo Cruz Mejía <@jackcm01>) que indicó cómo solucionar el problema. Al final solo había que sustituir el "true" por un "console.log". Y justo eso fue lo que hice ✨.

--- libs/sequelize.js
+++ libs/sequelize.js
@@ -7,7 +7,7 @@ const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${
 
 const sequelize = new Sequelize(URI, {
 	dialect: "postgres",
-	logging: true
+	logging: console.log
 });
 
 module.exports = sequelize;
