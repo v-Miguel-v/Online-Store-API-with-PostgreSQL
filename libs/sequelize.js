392e1530a2c678c21ec49c08f142513108c97697
Miguel Velásquez <vmiguel.xy@gmail.com>
2023-04-04 17:17:34 -0400

    Migraciones
    Se implementaron las Migraciones de Sequelize para realizar los cambios y actulizaciones de la base de datos.
    Ahora ya no usamos "sync", sino que directamente nuestra base de datos se actualiza y modifica a partir de las migraciones.
    
    Por ahora solo está la migración de la tabla de los usuarios, pero poco a poco se irán añadiendo las demás.

--- libs/sequelize.js
+++ libs/sequelize.js
@@ -4,14 +4,13 @@ const { config } = require("../config/config");
 
 const USER = encodeURIComponent(config.dbUser);
 const PASSWORD = encodeURIComponent(config.dbPassword);
-const URI = `mysql://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
 
 const sequelize = new Sequelize(URI, {
-	dialect: "mysql",
+	dialect: "postgres",
 	logging: console.log
 });
 
 setupModels(sequelize);
-sequelize.sync();
 
 module.exports = sequelize;
