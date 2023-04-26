1306b498478187e45ffd6415e442bb3047672ef3
Miguel Velásquez <vmiguel.xy@gmail.com>
2023-04-26 15:47:25 -0400

    Corrección en las migraciones.
    Habían algunos pequeños problemitas en las migraciones. Habían ciertos conflictos y errores que no permitían que las migraciones se ejecutaran de la forma correcta al momento de pasar la base de datos al servicio PostgresSQL de Railway. Pero ahora ya está solucionado.

--- db/migrations/20230423015823-create-orders-table.js
+++ db/migrations/20230423015823-create-orders-table.js
@@ -1,10 +1,35 @@
 "use strict";
-const { OrderSchema, ORDER_TABLE } = require("./../models/order.model");
+const { DataTypes, Sequelize } = require("sequelize");
+const { ORDER_TABLE } = require("./../models/order.model");
+const { CUSTOMER_TABLE } = require("./../models/customer.model");
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface) {
-    await queryInterface.createTable(ORDER_TABLE, OrderSchema);
+    await queryInterface.createTable(ORDER_TABLE, {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER
+      },
+      customerId: {
+        allowNull: false,
+        type: DataTypes.INTEGER,
+        field: "customer_id",
+        references: {
+          model: CUSTOMER_TABLE,
+          key: "id"
+        },
+        onUpdate: "CASCADE",
+        onDelete: "SET NULL"
+      },
+      date: {
+        allowNull: false,
+        type: DataTypes.DATE,
+        defaultValue: Sequelize.NOW
+      }
+    });
   },
 
   async down (queryInterface) {
